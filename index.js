de6fae0df97e808bc7515bb4489e27a35ec27ccf
Jon Jee <jydrogen@gmail.com>
2020-01-16 22:14:46 +0900

    feat: add store

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
@@ -26,7 +28,7 @@ function reducer(state = initialState, action) {
         case INCREASE:
             return {
                 ...state,
-                counter: state.counter + action.decrease
+                counter: state.counter + action.difference
             };
         case DECREASE:
             return {
@@ -36,4 +38,31 @@ function reducer(state = initialState, action) {
         default:
             return state;
     }
+}
+
+const store = createStore(reducer);
+
+const render = () => {
+    const state = store.getState();
+    if (state.toggle) {
+        divToggle.classList.add('active');
+    } else {
+        divToggle.classList.remove('active');
+    }
+    counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+divToggle.onclick = () => {
+    store.dispatch(toggleSwitch());
+}
+
+btnIncrease.onclick = () => {
+    store.dispatch(increase(1));
+}
+
+btnDecrease.onclick = () => {
+    store.dispatch(decrease());
 }
